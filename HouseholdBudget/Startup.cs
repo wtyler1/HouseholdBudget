f49b557ec1af2b153995a45441180c71c985040b
weltontylerii <weltontylerii@WELTONTYLER11F5>
2016-11-22 14:59:18 -0500

    Add project files.

---
+++ HouseholdBudget/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseholdBudget.Startup))]
+namespace HouseholdBudget
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
